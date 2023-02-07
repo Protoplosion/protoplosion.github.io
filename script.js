f6fd69c90a24cdf62f85e1ef175daac61f4a7d07
Protoplosion <54971638+Protoplosion@users.noreply.github.com>
2023-02-07 06:28:40 -0500

    Update script.js

--- script.js
+++ script.js
@@ -150,8 +150,7 @@ const SPEED_Z = 0.02; // rps
 const POINT3D = function(x, y, z) { this.x = x; this.y = y; this.z = z; };
 
 // set up the canvas and context
-var canvas = document.createElement("canvas");
-document.body.appendChild(canvas);
+var canvas = document.getElementById("render");
 var ctx = canvas.getContext("2d");
 
 // dimensions
