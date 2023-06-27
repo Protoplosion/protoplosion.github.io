2676351812e63d196f6c755de84443487b9fb311
Protoplosion <54971638+Protoplosion@users.noreply.github.com>
2023-06-27 16:07:09 -0400

    Update script.js

--- script.js
+++ script.js
@@ -149,7 +149,7 @@ function sup() {
         var noise = document.getElementById("noise")
         noise.play()
         setInterval(function () {
-            hue += 1
+            hue = Math.round(Math.random() * 359)
             document.getElementById("splashText").style.color = "hsl(" + hue + ", 100%, 50%)"
         }, 10)
     }
