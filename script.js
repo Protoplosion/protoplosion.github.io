4b42f7c28bad1cec6e8b4f3f0e2e45a04ceb83e3
Protoplosion <54971638+Protoplosion@users.noreply.github.com>
2023-06-27 15:30:28 -0400

    Update script.js

--- script.js
+++ script.js
@@ -145,97 +145,53 @@ function sup() {
 }
 
 
-const Point2D = function(x, y) { this.x = x; this.y = y; };
-const Point3D = function(x, y, z) { this.x = x; this.y = y; this.z = z; };
-const Cube = function(x, y, z, size) {
-    Point3D.call(this, x, y, z);
+const title = "Protoplosion"
+let count = 0
 
-    size *= 0.5;
+if (document.getElementById("title").innerHTML == title) clearInterval(titleWrite)
 
-    this.vertices = [
-        new Point3D(x - size, y - size, z - size),
-        new Point3D(x + size, y - size, z - size),
-        new Point3D(x + size, y + size, z - size),
-        new Point3D(x - size, y + size, z - size),
-        new Point3D(x - size, y - size, z + size),
-        new Point3D(x + size, y - size, z + size),
-        new Point3D(x + size, y + size, z + size),
-        new Point3D(x - size, y + size, z + size)
-    ];
-
-    this.faces = [
-        [0, 1, 2, 3],
-        [0, 4, 5, 1],
-        [1, 5, 6, 2],
-        [3, 2, 6, 7],
-        [0, 3, 7, 4],
-        [4, 7, 6, 5]
-    ];
-};
-
-Cube.prototype = {
-    rotateX:function(radian) {
-        var cosine = Math.cos(radian);
-        var sine   = Math.sin(radian);
-
-        for (let index = this.vertices.length - 1; index > -1; -- index) {
-            let p = this.vertices[index];
-
-            let y = (p.y - this.y) * cosine - (p.z - this.z) * sine;
-            let z = (p.y - this.y) * sine + (p.z - this.z) * cosine;
-
-            p.y = y + this.y;
-            p.z = z + this.z;
-        }
-    },
-    rotateY:function(radian) {
-        var cosine = Math.cos(radian);
-        var sine   = Math.sin(radian);
-
-        for (let index = this.vertices.length - 1; index > -1; -- index) {
-            let p = this.vertices[index];
-            let x = (p.z - this.z) * sine + (p.x - this.x) * cosine;
-            let z = (p.z - this.z) * cosine - (p.x - this.x) * sine;
-            p.x = x + this.x;
-            p.z = z + this.z;
-        }
-    }
-};
-
-var canvas = document.getElementById("canvas-background")
-var context = canvas.getContext("2d")
-var pointer = new Point2D(0, 0);
-var cube = new Cube(0, 0, 1000, 500);
-
-var height = document.documentElement.clientHeight;
-var width = document.documentElement.clientWidth;
-
-function project(points3d, width, height) {
-
-    var points2d = new Array(points3d.length);
-
-    var focal_length = 500;
-
-    for (let index = points3d.length - 1; index > -1; -- index) {
-        let p = points3d[index];
-
-        let x = p.x * (focal_length / p.z) + width * 0.5;
-        let y = p.y * (focal_length / p.z) + height * 0.5;
-
-        points2d[index] = new Point2D(x, y);
-    }
-    return points2d;
+let titleSet
+if (!titleSet) {
+    titleSet = true
+    var titleWrite = setInterval (() => {
+        document.getElementById("title").innerHTML += title.charAt(count)
+        count++
+    }, 100)
 }
 
-function loop() {
-    window.requestAnimationFrame(loop);
+const canvas = document.getElementById("canvas-background")
+const context = canvas.getContext("2d")
 
-    height = document.documentElement.clientHeight;
-    width = document.documentElement.clientWidth;
+const w = canvas.width = document.documentElement.clientHeight;
+const h = canvas.height = document.documentElement.clientWidth;
 
-    context.canvas.height = height;
-    context.canvas.width  = width;
-    
+//const w = canvas.width = document.body.offsetWidth
+//const h = canvas.height = document.body.offsetHeight
+
+context.fillStyle = "#000"
+context.fillRect(0, 0, w, h)
+
+const cols = Math.floor(w / 20) + 1
+const ypos = Array(cols).fill(0)
+
+const chars = title
+
+setInterval (() => {
+    context.fillStyle = "#0001"
+    context.fillRect(0, 0, w, h)
+  
+    context.fillStyle = "#0f0"
+    context.font = "15pt monospace"
+  
+    ypos.forEach((y, ind) => {
+      const char = chars[Math.floor(Math.random() * chars.length)]
+  
+      const x = ind * 20
+      context.fillText(char, x, y)
+  
+      if (y > 100 + Math.random() * 10000) ypos[ind] = 0
+      else ypos[ind] = y + 20
+
     if (document.getElementById("splashText").innerHTML == "Yes master?") {
         document.getElementById("canvas-background").style.backgroundImage = "url('waifu.jpg')";
         document.getElementById("canvas-background").style.backgroundSize = "cover";
@@ -244,69 +200,4 @@ function loop() {
         context.fillStyle = "#10101f";
         context.fillRect(0, 0, width, height);
     }
-
-    context.strokeStyle = "white";
-
-    cube.rotateX(pointer.y * 0.00015);
-    cube.rotateY(-pointer.x * 0.00015);
-    
-    if (supPressed) {
-        context.fillStyle = "hsl(" + hue + ", 100%, 50%)";
-    } else {
-        context.fillStyle = "red";
-    }
-
-    var vertices = project(cube.vertices, width, height);
-
-    for (let index = cube.faces.length - 1; index > -1; -- index) {
-
-        let face = cube.faces[index];
-        
-        let p1 = cube.vertices[face[0]];
-        let p2 = cube.vertices[face[1]];
-        let p3 = cube.vertices[face[2]];
-
-        let v1 = new Point3D(p2.x - p1.x, p2.y - p1.y, p2.z - p1.z);
-        let v2 = new Point3D(p3.x - p1.x, p3.y - p1.y, p3.z - p1.z);
-
-        let n  = new Point3D(v1.y * v2.z - v1.z * v2.y, v1.z * v2.x - v1.x * v2.z, v1.x * v2.y - v1.y * v2.x);
-
-        if (-p1.x * n.x + -p1.y * n.y + -p1.z * n.z <= 0) {
-            context.beginPath();
-            context.moveTo(vertices[face[0]].x, vertices[face[0]].y);
-            context.lineTo(vertices[face[1]].x, vertices[face[1]].y);
-            context.lineTo(vertices[face[2]].x, vertices[face[2]].y);
-            context.lineTo(vertices[face[3]].x, vertices[face[3]].y);
-            context.closePath();
-            context.fill();
-            context.stroke();
-        }
-    }
-}
-loop();
-(function() {
-    document.onmousemove = handleMouseMove;
-    function handleMouseMove(event) {
-        var eventDoc, doc, body;
-
-        event = event || window.event;
-        
-        if (event.pageX == null && event.clientX != null) {
-            eventDoc = (event.target && event.target.ownerDocument) || document;
-            doc = eventDoc.documentElement;
-            body = eventDoc.body;
-
-            event.pageX = event.clientX +
-            (doc && doc.scrollLeft || body && body.scrollLeft || 0) -
-            (doc && doc.clientLeft || body && body.clientLeft || 0);
-            event.pageY = event.clientY +
-            (doc && doc.scrollTop  || body && body.scrollTop  || 0) -
-            (doc && doc.clientTop  || body && body.clientTop  || 0 );
-        }
-
-        pointer.x = event.pageX - width * 0.5;
-        pointer.y = event.pageY - height * 0.5;
-    }
-})();
-
-//document.body.style.background = 'url(' + canvas.toDataURL() + ')';
+}, 50)
