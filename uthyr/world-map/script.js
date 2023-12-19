a0e3169921704452b0be14afb3cba51bd0cd9018
PrototypError <54971638+PrototypError@users.noreply.github.com>
2023-12-19 05:01:43 -0500

    stuff changed

--- uthyr/world-map/script.js
+++ uthyr/world-map/script.js
@@ -1,141 +1,297 @@
-const canvas = document.getElementById("map-canvas")
+function drawImage(image1){
+    var events = new Events("map-canvas");
+    var canvas = events.getCanvas();
+    var context = events.getContext();
 
-canvas.width = document.body.offsetWidth
-canvas.height = document.body.offsetHeight
+    var rectX = canvas.width / 2 - image1.width / 2;
+    var rectY = canvas.height / 2 - image1.height / 2;
+    var draggingRect = false;
+    var draggingRectOffsetX = 0;
+    var draggingRectOffsetY = 0;
 
-var gkhead = new Image
+    events.setDrawStage(function(){
+        var mousePos = this.getMousePos();
 
-window.onload = function () {
-
-    var ctx = canvas.getContext("2d");
-    trackTransforms(ctx);
-
-    function redraw() {
-        // Clear the entire canvas
-        var p1 = ctx.transformedPoint(0, 0);
-        var p2 = ctx.transformedPoint(canvas.width, canvas.height);
-        ctx.clearRect(p1.x, p1.y, p2.x - p1.x, p2.y - p1.y);
-
-        ctx.save();
-        ctx.setTransform(1, 0, 0, 1, 0, 0);
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
-        ctx.restore();
-
-        ctx.drawImage(gkhead, 0, 0);
-    }
-    redraw();
-
-    var lastX = canvas.width / 2, lastY = canvas.height / 2;
-
-    var dragStart, dragged;
-
-    canvas.addEventListener("mousedown", function (evt) {
-        document.body.style.mozUserSelect = document.body.style.webkitUserSelect = document.body.style.userSelect = "none";
-        lastX = evt.offsetX || (evt.pageX - canvas.offsetLeft);
-        lastY = evt.offsetY || (evt.pageY - canvas.offsetTop);
-        dragStart = ctx.transformedPoint(lastX, lastY);
-        dragged = false;
-    }, false);
-
-    canvas.addEventListener("mousemove", function (evt) {
-        lastX = evt.offsetX || (evt.pageX - canvas.offsetLeft);
-        lastY = evt.offsetY || (evt.pageY - canvas.offsetTop);
-        dragged = true;
-        if (dragStart) {
-            var pt = ctx.transformedPoint(lastX, lastY);
-            ctx.translate(pt.x - dragStart.x, pt.y - dragStart.y);
-            redraw();
+        if (draggingRect) {
+            rectX = mousePos.x - draggingRectOffsetX;
+            rectY = mousePos.y - draggingRectOffsetY;
         }
-    }, false);
 
-    canvas.addEventListener("mouseup", function (evt) {
-        dragStart = null;
-        if (!dragged) zoom(evt.shiftKey ? -1 : 1);
-    }, false);
+        // clear the canvas
+        this.clear();
 
-    var scaleFactor = 1.1;
+        this.beginRegion();
+        context.drawImage(image1, rectX, rectY, image1.width, image1.height);
+        // draw rectangular region for image
+        context.beginPath();
+        context.rect(rectX, rectY, image1.width, image1.height);
+        context.closePath();
 
-    var zoom = function (clicks) {
-        var pt = ctx.transformedPoint(lastX, lastY);
-        ctx.translate(pt.x, pt.y);
-        var factor = Math.pow(scaleFactor, clicks);
-        ctx.scale(factor, factor);
-        ctx.translate(-pt.x, -pt.y);
-        redraw();
-    }
+        this.addRegionEventListener("mousedown", function(){
+            draggingRect = true;
+            var mousePos = events.getMousePos();
 
-    var handleScroll = function (evt) {
-        var delta = evt.wheelDelta ? evt.wheelDelta / 40 : evt.detail ? -evt.detail : 0;
-        if (delta) zoom(delta);
-        return evt.preventDefault() && false;
+            draggingRectOffsetX = mousePos.x - rectX;
+            draggingRectOffsetY = mousePos.y - rectY;
+        });
+        this.addRegionEventListener("mouseup", function(){
+            draggingRect = false;
+        });
+        this.addRegionEventListener("mouseover", function(){
+            document.body.style.cursor = "pointer";
+        });
+        this.addRegionEventListener("mouseout", function(){
+            document.body.style.cursor = "default";
+        });
+
+        this.closeRegion();
+    });
+}
+
+window.onload = function(){
+    // load image
+    image1 = new Image();
+    image1.onload = function(){
+        drawImage(this);
     };
-
-    canvas.addEventListener("DOMMouseScroll", handleScroll, false);
-    canvas.addEventListener("mousewheel", handleScroll, false);
+    image1.src = "map.jpg";
 };
 
-gkhead.src = "map.jpg";
+class Events {
+    constructor(canvasId) {
+        this.canvas = document.getElementById(canvasId);
+        this.canvas.width = document.body.offsetWidth;
+        this.canvas.height = document.body.offsetHeight;
+        this.context = this.canvas.getContext("2d");
+        this.drawStage = undefined;
+        this.listening = false;
+        
+        // desktop flags
+        this.mousePos = null;
+        this.mouseDown = false;
+        this.mouseUp = false;
+        this.mouseOver = false;
+        this.mouseMove = false;
+        
+        // mobile flags
+        this.touchPos = null;
+        this.touchStart = false;
+        this.touchMove = false;
+        this.touchEnd = false;
+        
+        // Region Events
+        this.currentRegion = null;
+        this.regionIndex = 0;
+        this.lastRegionIndex = -1;
+        this.mouseOverRegionIndex = -1;
+    }
 
-// Adds ctx.getTransform() - returns an SVGMatrix
-// Adds ctx.transformedPoint(x,y) - returns an SVGPoint
-function trackTransforms(ctx) {
-    var svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
-    var xform = svg.createSVGMatrix();
-    ctx.getTransform = function () { return xform; };
+    getContext() {
+        return this.context;
+    }
 
-    var savedTransforms = [];
-    var save = ctx.save;
-    ctx.save = function () {
-        savedTransforms.push(xform.translate(0, 0));
-        return save.call(ctx);
-    };
+    getCanvas() {
+        return this.canvas;
+    }
 
-    var restore = ctx.restore;
-    ctx.restore = function () {
-        xform = savedTransforms.pop();
-        return restore.call(ctx);
-    };
+    clear() {
+        this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    }
 
-    var scale = ctx.scale;
-    ctx.scale = function (sx, sy) {
-        xform = xform.scaleNonUniform(sx, sy);
-        return scale.call(ctx, sx, sy);
-    };
+    getCanvasPos() {
+        let obj = this.getCanvas();
+        let top = 0;
+        let left = 0;
+        while (obj.tagName != "BODY") {
+            top += obj.offsetTop;
+            left += obj.offsetLeft;
+            obj = obj.offsetParent;
+        }
+        return {
+            top: top,
+            left: left
+        };
+    }
 
-    var rotate = ctx.rotate;
-    ctx.rotate = function (radians) {
-        xform = xform.rotate(radians * 180 / Math.PI);
-        return rotate.call(ctx, radians);
-    };
+    setDrawStage(func) {
+        this.drawStage = func;
+        this.listen();
+    }
 
-    var translate = ctx.translate;
-    ctx.translate = function (dx, dy) {
-        xform = xform.translate(dx, dy);
-        return translate.call(ctx, dx, dy);
-    };
+    reset(evt) {
+        if (!evt) {
+            evt = window.event;
+        }
+        
+        this.setMousePosition(evt);
+        this.setTouchPosition(evt);
+        this.regionIndex = 0;
+        
+        if (!this.animating && this.drawStage !== undefined) {
+            this.drawStage();
+        }
+        
+        // desktop flags
+        this.mouseOver = false;
+        this.mouseMove = false;
+        this.mouseDown = false;
+        this.mouseUp = false;
+        
+        // mobile touch flags
+        this.touchStart = false;
+        this.touchMove = false;
+        this.touchEnd = false;
+    }
 
-    var transform = ctx.transform;
-    ctx.transform = function (a, b, c, d, e, f) {
-        var m2 = svg.createSVGMatrix();
-        m2.a = a; m2.b = b; m2.c = c; m2.d = d; m2.e = e; m2.f = f;
-        xform = xform.multiply(m2);
-        return transform.call(ctx, a, b, c, d, e, f);
-    };
+    listen() {
+        const that = this;
+        
+        if (this.drawStage !== undefined) {
+            this.drawStage();
+        }
+        
+        // desktop events
+        this.canvas.addEventListener("mousedown", evt => {
+            that.mouseDown = true;
+            that.reset(evt);
+        }, false);
+        
+        this.canvas.addEventListener("mousemove", evt => {
+            that.reset(evt);
+        }, false);
+        
+        this.canvas.addEventListener("mouseup", evt => {
+            that.mouseUp = true;
+            that.reset(evt);
+        }, false);
+        
+        this.canvas.addEventListener("mouseover", evt => {
+            that.reset(evt);
+        }, false);
+        
+        this.canvas.addEventListener("mouseout", evt => {
+            that.mousePos = null;
+        }, false);
+        
+        // mobile events
+        this.canvas.addEventListener("touchstart", evt => {
+            evt.preventDefault();
+            that.touchStart = true;
+            that.reset(evt);
+        }, false);
+        
+        this.canvas.addEventListener("touchmove", evt => {
+            evt.preventDefault();
+            that.reset(evt);
+        }, false);
+        
+        this.canvas.addEventListener("touchend", evt => {
+            evt.preventDefault();
+            that.touchEnd = true;
+            that.reset(evt);
+        }, false);
+    }
 
-    var setTransform = ctx.setTransform;
-    ctx.setTransform = function (a, b, c, d, e, f) {
-        xform.a = a;
-        xform.b = b;
-        xform.c = c;
-        xform.d = d;
-        xform.e = e;
-        xform.f = f;
-        return setTransform.call(ctx, a, b, c, d, e, f);
-    };
+    getMousePos(evt) {
+        return this.mousePos;
+    }
 
-    var pt = svg.createSVGPoint();
-    ctx.transformedPoint = function (x, y) {
-        pt.x = x; pt.y = y;
-        return pt.matrixTransform(xform.inverse());
+    getTouchPos(evt) {
+        return this.touchPos;
+    }
+
+    setMousePosition(evt) {
+        const mouseX = evt.clientX - this.getCanvasPos().left + window.pageXOffset;
+        const mouseY = evt.clientY - this.getCanvasPos().top + window.pageYOffset;
+        this.mousePos = {
+            x: mouseX,
+            y: mouseY
+        };
+    }
+
+    setTouchPosition(evt) {
+        if (evt.touches !== undefined && evt.touches.length == 1) { // Only deal with one finger
+            const touch = evt.touches[0]; // Get the information for finger #1
+            const touchX = touch.pageX - this.getCanvasPos().left + window.pageXOffset;
+            const touchY = touch.pageY - this.getCanvasPos().top + window.pageYOffset;
+            
+            this.touchPos = {
+                x: touchX,
+                y: touchY
+            };
+        }
+    }
+
+    beginRegion() {
+        this.currentRegion = {};
+        this.regionIndex++;
+    }
+
+    addRegionEventListener(type, func) {
+        let event = (type.indexOf('touch') == -1) ? `on${type}` : type;
+        this.currentRegion[event] = func;
+    }
+
+    closeRegion() {
+        const pos = this.touchPos || this.mousePos;
+        
+        if (pos !== null && this.context.isPointInPath(pos.x, pos.y)) {
+            if (this.lastRegionIndex != this.regionIndex) {
+                this.lastRegionIndex = this.regionIndex;
+            }
+            
+            // handle onmousedown
+            if (this.mouseDown && this.currentRegion.onmousedown !== undefined) {
+                this.currentRegion.onmousedown();
+                this.mouseDown = false;
+            }
+            
+            // handle onmouseup
+            else if (this.mouseUp && this.currentRegion.onmouseup !== undefined) {
+                this.currentRegion.onmouseup();
+                this.mouseUp = false;
+            }
+            
+            // handle onmouseover
+            else if (!this.mouseOver && this.regionIndex != this.mouseOverRegionIndex && this.currentRegion.onmouseover !== undefined) {
+                this.currentRegion.onmouseover();
+                this.mouseOver = true;
+                this.mouseOverRegionIndex = this.regionIndex;
+            }
+            
+            // handle onmousemove
+            else if (!this.mouseMove && this.currentRegion.onmousemove !== undefined) {
+                this.currentRegion.onmousemove();
+                this.mouseMove = true;
+            }
+            
+            // handle touchstart
+            if (this.touchStart && this.currentRegion.touchstart !== undefined) {
+                this.currentRegion.touchstart();
+                this.touchStart = false;
+            }
+            
+            // handle touchend
+            if (this.touchEnd && this.currentRegion.touchend !== undefined) {
+                this.currentRegion.touchend();
+                this.touchEnd = false;
+            }
+            
+            // handle touchmove
+            if (!this.touchMove && this.currentRegion.touchmove !== undefined) {
+                this.currentRegion.touchmove();
+                this.touchMove = true;
+            }
+            
+        }
+        else if (this.regionIndex == this.lastRegionIndex) {
+            this.lastRegionIndex = -1;
+            this.mouseOverRegionIndex = -1;
+            
+            // handle mouseout condition
+            if (this.currentRegion.onmouseout !== undefined) {
+                this.currentRegion.onmouseout();
+            }
+        }
     }
 }
